16e49f4479fc6273564058b66f87d7a8eaa74240
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2021-02-19 19:03:22 +0100

    Adding webpage

---
+++ _R/compile_website.R
@@ -0,0 +1,16 @@
+#!/usr/bin/R
+
+## ==============================================================================
+## author          :Ghislain Vieilledent
+## email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+## web             :https://ghislainv.github.io
+## license         :GPLv3
+## ==============================================================================
+
+library(rmarkdown)
+library(here)
+
+# Compile site
+render_site(here())
+
+# EOF
