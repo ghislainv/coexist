7f706a403442bc7fa352ab2f9fc97fb9d85a999b
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2021-09-21 11:18:47 +0200

    Update

--- _R/compile_website.R
+++ _R/compile_website.R
@@ -13,4 +13,11 @@ library(here)
 # Compile site
 render_site(here())
 
+# Compile pdf
+# pdf
+# options(knitr.table.format="latex")
+# pdf_format <- bookdown::pdf_document2(citation_package="natbib", fig_caption=TRUE, keep_tex=FALSE, keep_md=FALSE,
+# 																			latex_engine="pdflatex", number_sections=TRUE, toc=FALSE)
+# bookdown::render_book(here("index.Rmd"), output_format=pdf_format)
+
 # EOF
